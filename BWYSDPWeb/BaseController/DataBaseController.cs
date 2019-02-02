76c01904540ea0ac2a17937b13fff835d712a66f
zhengyy <869601606@qq.com>
2019-02-02 17:22:40 +0800

    菜单设置

--- BWYSDPWeb/BaseController/DataBaseController.cs
+++ BWYSDPWeb/BaseController/DataBaseController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BWYSDPWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,20 +9,60 @@ namespace BWYSDPWeb.BaseController
 {
     public class DataBaseController : BaseController
     {
-        // GET: DataBase
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
         public DataBaseController()
         {
 
         }
+
+        [HttpGet]
+        public ActionResult LoadMenus()
+        {
+            List<Menu> mdata = new List<Menu>();
+            Menu m = new Menu();
+            m.MenuId = "0001";
+            m.MenuName = "主页";
+            m.ProgId = "Index";
+            m.Package = "Home";
+            mdata.Add(m);
+
+            m = new Menu();
+            m.MenuId = "0101";
+            m.MenuName = "供应链管理";
+            mdata.Add(m);
+
+            m = new Menu();
+            m.MenuId = "010101";
+            m.MenuName = "销售订单";
+            m.ProgId = "SaleOrder";
+            m.PmenuId = "0101";
+            m.Package = "SCM";
+            mdata.Add(m);
+
+            m = new Menu();
+            m.MenuId = "010102";
+            m.MenuName = "采购订单";
+            m.ProgId = "PurchaseOrder";
+            m.Package = "SCM";
+            m.PmenuId = "0101";
+            mdata.Add(m);
+
+            m = new Menu();
+            m.MenuId = "010103";
+            m.MenuName = "发货单";
+            m.ProgId = "ShipOrder";
+            m.Package = "SCM";
+            m.PmenuId = "0101";
+            mdata.Add(m);
+
+            return Json(new { Message = "success", data = mdata, Flag = 0 }, JsonRequestBehavior.AllowGet);
+        }
+
         /// <summary>
         /// 菜单跳转功能页
         /// </summary>
         /// <param name="progId"></param>
         /// <returns></returns>
+        [HttpGet]
         public ActionResult ConverToPage(string progId)
         {
             return View(progId);
@@ -31,9 +72,11 @@ namespace BWYSDPWeb.BaseController
         /// </summary>
         /// <param name="q"></param>
         /// <returns></returns>
+        [HttpGet]
         public ActionResult SearchFunc(string q)
         {
             return Json(new { message = "" }, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
---
+++ BWYSDPWeb/Models/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BWYSDPWeb.Models
+{
+    public class Menu
+    {
+        /// <summary>
+        /// 菜单ID
+        /// </summary>
+        public string MenuId { get; set; }
+        /// <summary>
+        /// 功能ID
+        /// </summary>
+        public string ProgId { get; set; }
+
+        /// <summary>
+        /// 所属包
+        /// </summary>
+        public string Package { get; set; }
+        /// <summary>
+        /// 菜单显示名称
+        /// </summary>
+        public string MenuName { get; set; }
+        /// <summary>
+        /// 父节点菜单ID
+        /// </summary>
+        public string PmenuId { get; set; }
+
+    }
+}
