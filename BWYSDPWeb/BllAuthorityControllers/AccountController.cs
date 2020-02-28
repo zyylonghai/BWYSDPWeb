15c10c77dc1d7ddcf67f2420057479e684afc594
zyy <960622133@qq.com>
2020-02-28 17:31:00 +0800

    2.28

--- BWYSDPWeb/BllAuthorityControllers/AccountController.cs
+++ BWYSDPWeb/BllAuthorityControllers/AccountController.cs
@@ -19,19 +19,20 @@ namespace BWYSDPWeb.BllAuthorityControllers
         protected override void BeforeSave()
         {
             base.BeforeSave();
-            string pwd = this.LibTables[0].Tables[0].DataTable .Rows[0]["Password"].ToString();
-            string confirmpwd = this.LibTables[0].Tables[0].DataTable.Rows[0]["Confirmpwd"].ToString();
-            if (string.Compare(pwd, confirmpwd, false) != 0)
-            {
-                //msg000000010 两次输入的密码不一致
-                //this.ThrowErrorException(AppCom.GetMessageDesc("msg000000010"));
-                this.AddMessage(AppCom.GetMessageDesc("msg000000010"));
-            }
+            //string pwd = this.LibTables[0].Tables[0].DataTable .Rows[0]["Password"].ToString();
+            //string confirmpwd = this.LibTables[0].Tables[0].DataTable.Rows[0]["Confirmpwd"].ToString();
+            var firstrow = this.LibTables[0].Tables[0].FindRow(0);
+            string pwd = firstrow.Password;
+            string confirmpwd = firstrow.Confirmpwd;
             if (this.OperatAction == OperatAction.Add)
             {
-                
+                if (string.Compare(pwd, confirmpwd, false) != 0)
+                {
+                    //msg000000010 两次输入的密码不一致
+                    //this.ThrowErrorException(AppCom.GetMessageDesc("msg000000010"));
+                    this.AddMessage(AppCom.GetMessageDesc("msg000000010"));
+                }
             }
-
         }
     }
 }
